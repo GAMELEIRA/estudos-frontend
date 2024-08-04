11b5202a5d47ee299a392b294e7bf1dd78576ed7
GAMELEIRA <gameleira270499@gmail.com>
2024-08-04 20:03:05 -0300

    feature(notificacoes-gols): Adiciona exercicio basico sobre notificacoes de gols do Brasil e Alemanha mais estilizacao

---
+++ 02-exercises/university/bandtec/programation-web/basic/12-exercise/scripts/notificacoes-gols.js
@@ -0,0 +1,13 @@
+const squadOne = 'Brasil';
+const squadTwo = 'Alemanha';
+let squadSelected = '';
+
+const showGol = (squad) => {
+    const element = document.createElement('p');
+    element.id = (squad === 1)? 'squad-one': 'squad-two';
+    squadSelected = (squad === 1)? `Gol do ${squadOne}`: ` Gol da ${squadTwo}`;
+    const newContent = document.createTextNode(squadSelected);
+    element.appendChild(newContent);
+    const currentDiv = document.getElementById('golsNotifications');
+    document.body.insertBefore(element, currentDiv);
+}
