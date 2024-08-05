961fa97dc516bf851faa8ea35b686e93d66bb7e4
GAMELEIRA <gameleira270499@gmail.com>
2024-08-05 13:16:04 -0300

    feature(humor-humano): Adiciona exercicio basico para exibicao de pizza conforme o tempo

---
+++ 02-exercises/university/bandtec/programation-web/basic/18-exercise/scripts/pizza.js
@@ -0,0 +1,13 @@
+const changePizza = (pizza) => {
+    const element = document.getElementById('pizza');
+    element.style = 'visibility: visible';
+    if (pizza === 1) {
+        element.src = 'images/pizza-8-pedacos.png';
+    } else if (pizza === 2) {
+        element.src = 'images/pizza-6-pedacos.png';
+    }  else if (pizza === 3) {
+        element.src = 'images/pizza-4-pedacos.png';
+    }  else {
+        element.src = 'images/restos-pizza.png';
+    }
+}
