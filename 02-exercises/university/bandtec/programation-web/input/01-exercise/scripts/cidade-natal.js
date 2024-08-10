f8292b27042c955378fcec771bd4de8fe0d288a5
GAMELEIRA <gameleira270499@gmail.com>
2024-08-09 23:20:23 -0300

    feature(cidade-natal): Adiciona exercicio que exibe formulario com data e cidade que a pessoa nasceu

---
+++ 02-exercises/university/bandtec/programation-web/input/01-exercise/scripts/cidade-natal.js
@@ -0,0 +1,5 @@
+const showAlert = () => {
+    const city = document.getElementById('city').value;
+    const date = document.getElementById('date').value; 
+    alert(`Você nasceu na cidade de ${city} em ${date}`);
+}
