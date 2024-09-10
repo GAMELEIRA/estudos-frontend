fc3b21bb0a1f212d4bb5f699e51e72b0b0ab3da7
GAMELEIRA <gameleira270499@gmail.com>
2024-09-10 11:37:50 -0300

    feat(project-10): Adiciona mecanismo de filtro para campo de busca

--- 04-projects/daily-challenge/10-project/src/scripts/index.js
+++ 04-projects/daily-challenge/10-project/src/scripts/index.js
@@ -1,18 +1,59 @@
-const persons = [
-    {
-        'name': 'Gabriel',
-        'image': './src/assets/images/person-1.png'
-    }, {
-        'name': 'Depay',
-        'image': './src/assets/images/person-2.png'
-    }, {
-        'name': 'Garro',
-        'image': './src/assets/images/person-3.png'
-    }, {
-        'name': 'Cibele',
-        'image': './src/assets/images/person-4.png'
-    }
-];
+let persons;
+
+const getPersons = () => {
+    persons = [
+        {
+            'name': 'Gabriel',
+            'image': './src/assets/images/person-1.png'
+        }, {
+            'name': 'Depay',
+            'image': './src/assets/images/person-2.png'
+        }, {
+            'name': 'Garro',
+            'image': './src/assets/images/person-3.png'
+        }, {
+            'name': 'Cibele',
+            'image': './src/assets/images/person-4.png'
+        }
+    ];
+}
+
+const filterPersons = () => {
+    const input = document.getElementById('search');
+    input.addEventListener('input', () => {
+        const value = input.value.toLowerCase();
+        console.log(input.value.length);
+
+        if (input.value) {
+            persons = persons.filter((res) => {
+                console.log(value === res.name, value, 'value', res.name, 'name');
+                return res.name.toLowerCase().includes(value);
+            });
+            console.log(persons, 'pessssaooooooasss');
+            console.log(!persons, 'tem que dar true')
+            console.log(persons, 'array');
+            const element = document.getElementsByClassName('line-person');
+            console.log(element, 'html');
+            [...element].forEach((item) => {
+                console.log(item, 'item do html')
+                item.remove();
+            });
+            listPersons();
+            if (!persons.length) {
+                getPersons();
+            }
+        } else {
+            const element = document.getElementsByClassName('line-person');
+            [...element].forEach((item) => {
+                console.log(item, 'item do html')
+                item.remove();
+            });
+            getPersons();
+            listPersons();
+        }
+    });
+
+}
 
 const listPersons = () => {
     const element = document.getElementById('list-persons');
@@ -52,8 +93,10 @@ const listPersons = () => {
     });
 }
 
+const main = () => {
+    getPersons();
+    listPersons();
+    filterPersons();
+}
 
-
-
-
-listPersons();
+main();
