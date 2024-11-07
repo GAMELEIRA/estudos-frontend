fa2287b81ee75581c1191d84f1e6d66683e66881
GAMELEIRA <gameleira270499@gmail.com>
2024-11-07 20:20:38 -0300

    feat(sayajin): Adiciona exercicio de matematica basica para calcular forca de um sayajin

---
+++ 02-exercises/algorithms/university/bandtec/02-mathematical-operations/02.01-simple-operations/22-exercise/scripts/index.js
@@ -0,0 +1,40 @@
+const tranformSsj = (sayajin = 0, strength = 0) => {
+  const sayajinOne = 50;
+  const sayajinTwo = 100;
+  const sayajinThree = 200;
+
+  switch (sayajin) {
+    case 1:
+      return {
+        name: 'SSJ',
+        ki: sayajinOne * strength,
+      };
+    case 2:
+      return {
+        name: 'SSJ',
+        ki: sayajinTwo * strength,
+      };
+    case 3:
+      return {
+        name: 'SSJ3',
+        ki: sayajinThree * strength,
+      };
+    default:
+      return {
+        name: 'undefined',
+        ki: strength,
+      };
+  }
+};
+
+const showMessage = (sayajin) => {
+  const element = document.getElementById('message');
+  console.log(sayajin);
+  element.innerText = `Após se transformar em ${sayajin.name}, o ki desse Sayajin passa a ser de ${sayajin.ki}`;
+};
+
+const requestTranformSsj = (type = 0) => {
+  const strength = Number(document.getElementById('strength').value);
+  const sayajin = tranformSsj(type, strength);
+  showMessage(sayajin);
+};
