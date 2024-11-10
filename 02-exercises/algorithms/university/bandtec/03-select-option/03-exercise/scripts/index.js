b7d81ac1b2a5ed37b964a173917238bb9a79c740
GAMELEIRA <gameleira270499@gmail.com>
2024-11-09 22:12:28 -0300

    feat(selecao-frases): Adiciona exercicio basico para selecao de frases de um usuario

---
+++ 02-exercises/algorithms/university/bandtec/03-select-option/03-exercise/scripts/index.js
@@ -0,0 +1,5 @@
+const showPhrase = () => {
+  const phraseOne = document.getElementById('phrase-one').value;
+  const phraseTwo = document.getElementById('phrase-two').value;
+  alert(phraseOne.concat(' ').concat(phraseTwo));
+};
