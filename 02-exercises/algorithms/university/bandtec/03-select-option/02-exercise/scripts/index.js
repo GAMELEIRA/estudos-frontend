a32f8e1d1769f0beaf394e67400742535cc96a93
GAMELEIRA <gameleira270499@gmail.com>
2024-11-09 21:25:14 -0300

    feat(entretenimento): Adiciona exercicio basico para a selecao de um tipo de entretenimento

---
+++ 02-exercises/algorithms/university/bandtec/03-select-option/02-exercise/scripts/index.js
@@ -0,0 +1,27 @@
+const showMessage = (message = '') => {
+  alert(message);
+};
+const requestSeeEntertainment = () => {
+  const movies = [
+    {
+      type: 'Cinema',
+      message: 'Assistir a filme "Marley & Eu"',
+    },
+    {
+      type: 'Teatro',
+      message: 'Assistir a peça "O Fantasma da Ópera"',
+    },
+    {
+      type: 'Parque',
+      message: ' Ir ao parque "Hopi Hari"',
+    },
+    {
+      type: 'Show',
+      message: 'Ouvir o show da "Lady Gaga"',
+    },
+  ];
+  const type = document.getElementById('entertainment').value;
+  const foundMovies = movies.find((movie) => movie.type === type);
+  console.log(foundMovies);
+  showMessage(foundMovies.message);
+};
