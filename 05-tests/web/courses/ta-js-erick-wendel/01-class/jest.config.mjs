ffe2268e7f5bd731c1e3268b548c1a44982f6faa
GAMELEIRA <gameleira270499@gmail.com>
2024-11-14 14:08:53 -0300

    feat(tests): Adiciona aprendizado inicial com a configuracao de projetos para testes

---
+++ 05-tests/web/courses/ta-js-erick-wendel/01-class/jest.config.mjs
@@ -0,0 +1,11 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ */
+
+/** @type {import('jest').Config} */
+const config = {
+  clearMocks: true,
+  coverageProvider: 'v8',
+};
+
+export default config;
