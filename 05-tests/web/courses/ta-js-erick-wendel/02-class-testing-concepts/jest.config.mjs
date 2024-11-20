78119d12aa75c9f0e0ae4f8b5ba2a3942aaaee5e
GAMELEIRA <gameleira270499@gmail.com>
2024-11-20 17:26:09 -0300

    feat(testing-concepts): Adiciona casos de teste para a classe Person

---
+++ 05-tests/web/courses/ta-js-erick-wendel/02-class-testing-concepts/jest.config.mjs
@@ -0,0 +1,12 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ */
+
+/** @type {import('jest').Config} */
+const config = {
+  clearMocks: true,
+  coverageProvider: 'v8',
+  transform: {},
+};
+
+export default config;
---
+++ 05-tests/web/courses/ta-js-erick-wendel/02-class-testing-concepts/src/index.js
@@ -0,0 +1,3 @@
+import { Person } from './models/Person.model';
+
+const person = new Person('Gabriel', 25, '12345678900');
