2e88fb3333fd75d99d2ee41d9bfefaf035709214
GAMELEIRA <gameleira270499@gmail.com>
2024-12-11 06:59:59 -0300

    feat(types-primitives): Adiciona aprendizado com tipos primitivos em ts

--- 01-fundamentals/01-platforms/web/04-typescript/02-courses/01-essential-training-typescript/02-basic-types/01-primitive-types/index.ts
+++ 01-fundamentals/01-platforms/web/04-typescript/02-courses/01-essential-training-typescript/02-basic-types/01-primitive-types/index.ts
@@ -2,7 +2,7 @@
 /**TIPAGEM DE VARIAVEIS */
 const nome: string = 'Gabriel';
 const idade: number = 20;
-const programador: boolean = true;
+const programador: boolean = true; 
 
 /**TIPAGEM PARA A ASSINATURA DO METODO, NOS PARAMETROS E RETORNO */
 function dizerOla(nome: string): void {
@@ -15,3 +15,31 @@ const simbolo: Symbol = Symbol();
 
 /*+++++++++++++++++++++++++++ INFERENCIA DE TIPOS +++++++++++++++++++++++++++++++*/
 let sobrenome = 'Gameleira';
+
+/*+++++++++++++++++++++++++++++TIPOS LITERAIS++++++++++++++++++++++++ */
+//UM TIPO LITERAL EH UTILIZADO PARA DAR UM ESCOPO FIXO PARA UMA VARIAVEL
+//ELE SERA UM SUBCONJUTO DO TIPO PRIMITIVO
+let tv: 'SBT' = "SBT";
+let PI: 3.14 = 3.14;
+
+//TIPO GENERICO
+const clube: string = 'Corinthians';
+
+//QUALQUER TIPO PRIMITIVO EH UM SUJCONJUNTO DE UNKNOWN
+const numero3: unknown = 1;
+const nome1: unknown = 'Gabriel';
+const teraPizzahHoje: unknown = true; 
+
+{
+    const nome: string = 'Gabriel'
+    let comida: {
+        nome: string
+        preco?: string | number; //OPCIONAL E PODE ASSUMIR DOIS TIPOS NUMERO OU STRING
+        dizerNome: () => 'Gabriel'
+    };
+    
+    //GARANTO QUE A VARIAVEL NAO SERA UNDEFINED E DEIXO OPCIONAL O TO STRING CASO O PRECO SEJA NUMERICO
+    console.log(comida!.preco?.toString())
+}
+
+const test = (a: any) => {} 
